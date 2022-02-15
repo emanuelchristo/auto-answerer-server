e643141f07646d8d06f82d6f79ce356cbf898d97
emanuelchristo <emanuelchristochris@gmail.com>
2022-02-15 23:28:35 +0530

    special character replce

--- get-keywords.js
+++ get-keywords.js
@@ -2,6 +2,7 @@ const rp = require('request-promise')
 const unique = require('array-unique')
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
+const replaceSpecialCharacters = require('replace-special-characters')
 
 const commonWords = require('./common-words.js')
 
@@ -13,11 +14,17 @@ async function getKeywords(url, selector, callback) {
 			let s = selector || 'p a'
 			let alllinks = dom.window.document.querySelectorAll(s)
 			let heading = dom.window.document.querySelector('h1').textContent
-			keywords.push(heading.replace(/[^a-zA-Z0-9]/g, '').toLowerCase())
+			keywords.push(
+				replaceSpecialCharacters(heading)
+					.replace(/[^a-zA-Z0-9]/g, '')
+					.toLowerCase()
+			)
 			for (a of alllinks) {
 				let linkname = a.innerHTML
 				if (!contains(linkname, ['[', '<', '(', ')', 'season'])) {
-					let temp = linkname.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+					let temp = replaceSpecialCharacters(linkname)
+						.replace(/[^a-zA-Z0-9]/g, '')
+						.toLowerCase()
 					if (temp != '' && temp.length < 31 && temp.length > 4)
 						if (!commonWords.includes(temp)) keywords.push(temp)
 				}
